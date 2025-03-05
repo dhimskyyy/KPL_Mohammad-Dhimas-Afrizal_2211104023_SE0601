d5e263a2777946c4cd4b1c01b4c254ea3fe9e36f
Mohammad Dhimas Afrizal <mdhimas25@gmail.com>
2025-03-05 16:28:28 +0700

    jurnal

---
+++ 02_Pengenalan_IDE_dan_Pemrograman_CSharp/jurnal/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Implementasi C: Memeriksa bilangan prima
+        Console.Write("Masukkan sebuah angka (1-10000): ");
+        int nilaiInt = Convert.ToInt32(Console.ReadLine());
+
+        if (IsPrime(nilaiInt))
+        {
+            Console.WriteLine($"Angka {nilaiInt} merupakan bilangan prima");
+        }
+        else
+        {
+            Console.WriteLine($"Angka {nilaiInt} bukan merupakan bilangan prima");
+        }
+    }
+
+    // Fungsi untuk mengecek bilangan prima
+    static bool IsPrime(int number)
+    {
+        if (number < 2) return false;
+        for (int i = 2; i * i <= number; i++)
+        {
+            if (number % i == 0) return false;
+        }
+        return true;
+    }
+}

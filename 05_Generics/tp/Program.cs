8541aa8ad029b6394705bbe9134e1f79a8c35f9f
Mohammad Dhimas Afrizal <mdhimas25@gmail.com>
2025-03-12 16:41:36 +0700

    Melakukan commit dengan pesan "menambahkan class haloGeneric dengan method SapaUser"

---
+++ 05_Generics/tp/Program.cs
@@ -0,0 +1,32 @@
+using System;
+
+public class HaloGeneric
+{
+    public static void SapaUser<T>(T user)
+    {
+        Console.WriteLine($"Halo Bro {user}");
+    }
+}
+
+public class DataGeneric<T>
+{
+    private T data;
+    public DataGeneric(T data)
+    {
+        this.data = data;
+    }
+    public void PrintData()
+    {
+        Console.WriteLine($"Data yang tersimpan adalah: {data}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        HaloGeneric.SapaUser("Mohammad Dhimas Afrizal");
+        DataGeneric<string> data = new DataGeneric<string>("2211104023");
+        data.PrintData();
+    }
+}

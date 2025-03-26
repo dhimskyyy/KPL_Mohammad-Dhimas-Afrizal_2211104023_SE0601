d50fa2bb215f46761043478c764eb58d5c80df60
Mohammad Dhimas Afrizal <mdhimas25@gmail.com>
2025-03-27 00:36:36 +0700

    08

---
+++ 08_Runtime_Configuration_dan_Internationalization/CovidConfig.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+
+public class CovidConfig
+{
+    public string SatuanSuhu { get; set; }
+    public int BatasHariDeman { get; set; }
+    public string PesanDitolak { get; set; }
+    public string PesanDiterima { get; set; }
+
+    private const string ConfigFile = "covid_config.json";
+
+    public CovidConfig()
+    {
+        LoadConfig();
+    }
+
+    private void LoadConfig()
+    {
+        if (File.Exists(ConfigFile))
+        {
+            string json = File.ReadAllText(ConfigFile);
+            var config = JsonConvert.DeserializeObject<CovidConfig>(json);
+
+            SatuanSuhu = config.SatuanSuhu;
+            BatasHariDeman = config.BatasHariDeman;
+            PesanDitolak = config.PesanDitolak;
+            PesanDiterima = config.PesanDiterima;
+        }
+        else
+        {
+            SatuanSuhu = "celcius";
+            BatasHariDeman = 14;
+            PesanDitolak = "Anda tidak diperbolehkan masuk ke dalam gedung ini";
+            PesanDiterima = "Anda dipersilahkan untuk masuk ke dalam gedung ini";
+
+            SaveConfig(); 
+        }
+    }
+
+    public void SaveConfig()
+    {
+        var json = JsonConvert.SerializeObject(this, Formatting.Indented);
+        File.WriteAllText(ConfigFile, json);
+    }
+
+    public void UbahSatuan()
+    {
+        SatuanSuhu = (SatuanSuhu == "celcius") ? "fahrenheit" : "celcius";
+        SaveConfig();
+    }
+}
---
+++ 08_Runtime_Configuration_dan_Internationalization/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul8_2211104023
+{
+    class Program
+{
+    static void Main()
+    {
+        CovidConfig config = new CovidConfig();
+
+        Console.WriteLine($"Berapa suhu badan anda saat ini? Dalam nilai {config.SatuanSuhu}: ");
+        double suhu = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Berapa hari yang lalu (perkiraan) anda terakhir memiliki gejala demam?");
+        int hariDeman = Convert.ToInt32(Console.ReadLine());
+
+        bool suhuValid = (config.SatuanSuhu == "celcius" && suhu >= 36.5 && suhu <= 37.5) ||
+                         (config.SatuanSuhu == "fahrenheit" && suhu >= 97.7 && suhu <= 99.5);
+
+        bool hariDemanValid = hariDeman < config.BatasHariDeman;
+
+        Console.WriteLine(suhuValid && hariDemanValid ? config.PesanDiterima : config.PesanDitolak);
+        Console.WriteLine("\nApakah ingin mengubah satuan suhu? (y/n)");
+        string pilihan = Console.ReadLine().ToLower();
+        if (pilihan == "y")
+        {
+            config.UbahSatuan();
+            Console.WriteLine($"Satuan suhu diubah menjadi: {config.SatuanSuhu}");
+        }
+    }
+}
+}

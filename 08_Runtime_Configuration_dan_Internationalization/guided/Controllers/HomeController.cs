ad16c185898ba827cfbb62d57a75d8258cf72825
Mohammad Dhimas Afrizal <mdhimas25@gmail.com>
2025-04-17 11:31:26 +0700

    localization

---
+++ 08_Runtime_Configuration_dan_Internationalization/guided/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using localizationnnn.Models;
+using localizationnnn.Services;
+using Microsoft.AspNetCore.Localization;
+
+namespace localizationnnn.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private LanguageService _localizationnnn;
+        public HomeController(ILogger<HomeController> logger, LanguageService localization)
+        {
+            _localizationnnn = localization;
+            _logger = logger;
+        }
+        public IActionResult Index()
+        {
+            ViewBag.WelcomeMessage = _localizationnnn.Getkey("str_welcome_message").Value;
+            //get culture information
+            var currentCulture = Thread.CurrentThread.CurrentUICulture.Name;
+            return View();
+        }
+        #region Localization
+        public IActionResult ChangeLanguage(string culture)
+        {
+            Response.Cookies.Append(CookieRequestCultureProvider.DefaultCookieName, CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture)), new CookieOptions()
+            {
+                Expires = DateTimeOffset.UtcNow.AddYears(1)
+            });
+
+            return Redirect(Request.Headers["Referer"].ToString());
+        }
+        #endregion
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel
+            {
+                RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
+            });
+        }
+    }
+}
---
+++ 08_Runtime_Configuration_dan_Internationalization/guided/Services/LanguangeServices.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.Extensions.Localization;
+using System.Reflection;
+
+namespace localizationnnn.Services
+{
+    public class SharedResource
+    {
+    }
+
+    public class LanguageService
+    {
+        private readonly IStringLocalizer _localizer;
+
+        public LanguageService(IStringLocalizerFactory factory)
+        {
+            var type = typeof(SharedResource);
+            var assemblyName = new AssemblyName(type.GetTypeInfo().Assembly.FullName);
+            _localizer = factory.Create("SharedResource", assemblyName.Name);
+        }
+
+        public LocalizedString Getkey(string key)
+        {
+            return _localizer[key];
+        }
+    }
+}
